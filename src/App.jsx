e820f24abe9a4a74bca5c031834b01dffce5e81c
binayakbhandari <binayakbhandari60@gmail.com>
2025-04-09 09:14:49 +0545

    Updated Indrajit Sir's Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Indrajit Sir's Portfolio is Under Maintenance</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Will be avaliable soon ...
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <pre>
+        Portfolio will be built in vite and React.<br />
+        So, Click on Vite and React logos to learn more
+        </pre>
       </p>
     </>
   )
